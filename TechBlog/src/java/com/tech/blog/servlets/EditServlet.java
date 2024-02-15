e847f6717bfe204c0307608c6476c6d871b7f997
Bridge Carson <132212030+heyDevhere@users.noreply.github.com>
2024-02-15 19:31:24 +0530

    Add files via upload

---
+++ TechBlog/src/java/com/tech/blog/servlets/EditServlet.java
@@ -0,0 +1,138 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.tech.blog.servlets;
+
+import com.tech.blog.dao.UserDao;
+import com.tech.blog.entities.Message;
+import com.tech.blog.entities.User;
+import com.tech.blog.helper.ConnectionProvider;
+import com.tech.blog.helper.Helper;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+
+/**
+ *
+ * @author hp
+ */
+@MultipartConfig
+public class EditServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String userEmail = request.getParameter("user_email");
+            String userName = request.getParameter("user_name");
+            String userPassword = request.getParameter("user_password");
+            String userAbout = request.getParameter("user_about");
+            Part part = request.getPart("image");
+            String imageName = part.getSubmittedFileName();
+            
+            HttpSession session = request.getSession();
+            User user = (User) session.getAttribute("currentUser");
+            
+            user.setEmail(userEmail);
+            user.setName(userName);
+            user.setPassword(userPassword);
+            user.setAbout(userAbout);
+            String oldFile = user.getProfile();
+
+            user.setProfile(imageName);
+
+            UserDao userDao = new UserDao(ConnectionProvider.getConnection());
+            boolean isUpdated = userDao.updateUser(user);
+            
+            if (isUpdated) {
+                // Save the new image
+                String uploadPath = getServletContext().getRealPath("") + File.separator + "pics";
+                File uploadDir = new File(uploadPath);
+                if (!uploadDir.exists()) {
+                    uploadDir.mkdir();
+                }
+                try (InputStream inputStream = part.getInputStream()) {
+                    Helper.saveFile(inputStream, uploadPath + File.separator + imageName);
+                }
+
+                // Delete the old image if it's not the default one
+                if (!oldFile.equals("default.png")) {
+                    File oldImageFile = new File(uploadPath + File.separator + oldFile);
+                    if (oldImageFile.exists()) {
+                        oldImageFile.delete();
+                    }
+                }
+
+                // Set success message
+                Message msg = new Message("Profile details updated successfully.", "success", "alert-success");
+                session.setAttribute("msg", msg);
+            } else {
+                // Set error message
+                Message msg = new Message("Failed to update profile details.", "error", "alert-danger");
+                session.setAttribute("msg", msg);
+            }
+            
+            response.sendRedirect("profile.jsp");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
